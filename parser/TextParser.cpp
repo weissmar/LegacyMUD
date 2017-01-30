57900ab5769dd22883a0c305f68486f92471afc7
David Rigert <rigertd@onid.orst.edu>
2017-01-29 17:14:54 -0800

    Wrote stubs for TextParser functions.

---
+++ parser/TextParser.cpp
@@ -0,0 +1,28 @@
+/*!
+  \file    TextParser.cpp
+  \author  David Rigert
+  \date    01/29/2017
+  \course  CS467, Winter 2017
+ 
+  \details This file contains the implementation code for the TextParser class.
+*/
+
+#include "TextParser.h"
+
+namespace legacymud { namespace parser {
+
+TextParser::TextParser() {
+
+}
+
+TextParser::~TextParser() {
+
+}
+
+// Converts text input from players into potential commands to be run.
+TextParseStatus TextParser::parse(const std::string &input, const VerbMap &areaVerbs, std::list<TextParseResult> &candidates) {
+    return TextParseStatus::INVALID_VERB;
+}
+
+
+}}
