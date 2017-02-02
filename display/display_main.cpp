06eb8f739a7a3d1356589a1ecee77e6c6793f163
Keith Adkins <adkinske@flip2.engr.oregonstate.edu>
2017-02-02 14:30:14 -0800

    Display main commit

---
+++ display/display_main.cpp
@@ -0,0 +1,41 @@
+/*************************************************************************
+ * Author:        Keith Adkins
+ * Date Created:  1/30/2017
+ * Last Modified: 1/30/2017
+ * Course:        CS467, Winter 2017
+ * Filename:      display_main.cpp
+ *
+ * Overview:
+ *     This main function for for local testing of the display class.
+ ************************************************************************/
+
+#include "Display.h"
+#include <string>
+#include <iostream>
+
+int main() {   
+    legacymud::display::Display d;
+    
+    std::string str1 = "Hello";
+    std::cout << d.addStyle(str1,d.YELLOW) << std::endl;
+    
+    std::string str2 = "This is a string.";
+    std::cout << d.addStyle(str2,d.CYAN, d.BRIGHT) << std::endl;
+    
+    std::string str3 = "This is a string.";
+    std::cout << d.addStyle(str3,d.CYAN, d.UNDERSCORE) << std::endl;
+    
+    std::string str4 = "This is a string.";
+    std::cout << d.addStyle(str4,d.WHITE, d.UNDERSCORE, d.BRIGHT) << std::endl;  
+
+    std::string str5 = "This is a string.";
+    std::cout << d.addStyle(str5,d.RED, d.WHITE) << std::endl; 
+
+    std::string str6 = "This is a string.";
+    std::cout << d.addStyle(str5,d.RED, d.WHITE, d.BRIGHT) << std::endl;   
+
+    std::string str7 = "This is a string.";
+    std::cout << d.addStyle(str7,d.RED, d.BLACK, d.UNDERSCORE, d.BRIGHT) << std::endl;    
+
+    return 0;
+}
