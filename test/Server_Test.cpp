ac6b2225d34b1a6df84eda962421449c632157e0
Keith Adkins <adkinske@flip2.engr.oregonstate.edu>
2017-02-11 21:23:44 -0800

    Added server unit test.

--- test/Server_Test.cpp
+++ test/Server_Test.cpp
@@ -6,35 +6,244 @@
  * Filename:      Server_Test.cpp
  *
  * Overview:
- *     Unit test for the Server class.
+ *     Unit tests for the Server class.  
  ************************************************************************/
 
 #include "Server.hpp"
 #include "GameLogic.hpp"    // game logic stub
 #include <string>
 #include <iostream>
-
+#include <stdlib.h>
+#include <time.h>
+#include <thread> 
+          
 #include <gtest/gtest.h>
 
 namespace {
 
-/* Server test. */
-TEST(ServerTest, InitServer) {
+
+/* Server constructor and getter testing. */
+TEST(ServerTest, ConstructServer) {
+    legacymud::telnet::Server ts;
+
+    /* Get server pause value. */
+    EXPECT_EQ(false, ts.getServerPause() ) 
+            << "Expect false for server pause default value.";
+
+    /* Get server max player value. */
+    EXPECT_EQ(0, ts.getMaxPlayers() ) 
+            << "Expect 0 for max player default value.";            
+
+    /* Get server player count value. */
+    EXPECT_EQ(0, ts.getPlayerCount() ) 
+            << "Expect 0 for player count starting value.";              
+
+    /* Get server timeout value. */
+    EXPECT_EQ(0, ts.getTimeOut() ) 
+            << "Expect 0 for timeout default value.";
+
+    /* Get server port value. */
+    EXPECT_EQ(0, ts.getServerPort() ) 
+            << "Expect 0 for server port default value.";
+
+    /* Get game logic pointer value. */
+    EXPECT_EQ(0, ts.getGameLogicPt() ) 
+            << "Expect 0 for game logic pointer default value.";
+            
+}
+
+/* Initialize server testing. */
+TEST(ServerTest, InitializeServer) {
     legacymud::telnet::Server ts;
     legacymud::engine::GameLogic gl;
-    int serverPort = 0;
-    int playerCap = 10;
-    int timeOut = 900;           // in seconds.
-    bool err;
-
-    /* Initialize the server. */
-    err = ts.initServer(serverPort, playerCap, timeOut, &gl);
+    int serverPort;
+    int playerCap;
+    int timeOut;           // timeout in seconds.
     
-    EXPECT_EQ(false, err) 
+    srand(time(NULL));     // seed the random number generator
+
+    /* All parameters are valid */
+    playerCap = 1;
+    timeOut = 1; 
+    serverPort = rand() % (65535-1000) + 1000;  // range 1000 to 65535
+    EXPECT_EQ(true, ts.initServer(serverPort, playerCap, timeOut, &gl) ) 
+            << "Expect true for all parameters being valid.  Could return false if port is already taken.";
+    
+    /* Shut the server down. */
+    EXPECT_EQ(true, ts.shutdown() ) 
+        << "Expect true that the server is shutdown.";
+    
+    /* Invalid server port of 999. */
+    serverPort = 999;
+    EXPECT_EQ(false, ts.initServer(serverPort, playerCap, timeOut, &gl) ) 
             << "Expect false for invalid port.";
-  
+   
+    /* Invalid server port of 65536. */
+    serverPort = 65536; 
+    EXPECT_EQ(false, ts.initServer(serverPort, playerCap, timeOut, &gl) ) 
+            << "Expect false for invalid port.";
+            
+    /* Invalid player cap. */
+    serverPort = rand() % (65535-1000) + 1000;  // range 1000 to 65535
+    playerCap = 0;
+    EXPECT_EQ(false, ts.initServer(serverPort, playerCap, timeOut, &gl) ) 
+            << "Expect false for invalid player cap.  Could return false if port is already taken."; 
+
+
+    /* Invalid timeout. */
+    serverPort = rand() % (65535-1000) + 1000;  // range 1000 to 65535
+    timeOut = 0;
+    EXPECT_EQ(false, ts.initServer(serverPort, playerCap, timeOut, &gl) ) 
+            << "Expect false for invalid timeout.  Could return false if port is already taken.";             
 }
 
 
+/* Server listen testing. */
+TEST(ServerTest, ListeningServer) {
+    legacymud::telnet::Server ts;
+    legacymud::engine::GameLogic gl;
+    int serverPort;
+    int playerCap=1;
+    int timeOut=1;           // timeout in seconds.
+    
+    srand(time(NULL));     // seed the random number generator
+
+    /* All parameters are valid */ 
+    serverPort = rand() % (65535-1000) + 1000;  // range 1000 to 65535
+    
+    ASSERT_EQ(ts.initServer(serverPort, playerCap, timeOut, &gl), true ) 
+            << "Expect true for initializing server.  Could return false if port is already taken.";
+    
+    /* The following only runs if the previous passes. */
+    
+    /* Send listening off to it's own thread. */
+    std::thread serverThread(&legacymud::telnet::Server::startListening, &ts);   
+    
+    /* Shut the server down. */
+    EXPECT_EQ(true, ts.shutdown() ) 
+        << "Expect true that the server is shutdown.";  
+        
+    serverThread.join();          
+}
+
+
+/* Server player disconnect and sending and receiving messages. */
+TEST(ServerTest, HandlingPlayers) {
+    legacymud::telnet::Server ts;
+    legacymud::engine::GameLogic gl;
+    int serverPort;
+    int playerCap=1;
+    int timeOut=1;           // timeout in seconds.
+    int playerFd = 1;
+    std::string inMsg;
+    
+    srand(time(NULL));     // seed the random number generator
+
+    /* All parameters are valid */ 
+    serverPort = rand() % (65535-1000) + 1000;  // range 1000 to 65535
+    
+    ASSERT_EQ(ts.initServer(serverPort, playerCap, timeOut, &gl), true ) 
+            << "Expect true for initializing server.  Could return false if port is already taken.";
+    
+    /* The following only runs if the previous passes. */
+    
+    /* Send listening off to it's own thread. */
+    std::thread serverThread(&legacymud::telnet::Server::startListening, &ts);   
+    
+    /* Disconnect a player.  There are no players so this should return false. */
+    EXPECT_EQ(false, ts.disconnectPlayer(playerFd) ) 
+        << "Expect false since there are no players.";
+        
+    /* Send a message to a player. There are no players so this should return false. NEWLINE. */
+    EXPECT_EQ(false, ts.sendMsg(playerFd, "Sending this message.", ts.NEWLINE) ) 
+        << "Expect false since there are no players.";
+
+    /* Send a message to a player. There are no players so this should return false. NO_NEWLINE. */
+    EXPECT_EQ(false, ts.sendMsg(playerFd, "Sending this message.", ts.NO_NEWLINE) ) 
+        << "Expect false since there are no players.";        
+        
+
+    /* Receive a message from a player. There are no players so this should return false.*/
+    EXPECT_EQ(false, ts.receiveMsg(playerFd, inMsg ) ) 
+        << "Expect false since there are no players.";         
+
+    /* Listen for messages from a player. There are no players so this should return false.*/
+    EXPECT_EQ(false, ts.listenForMsgs(playerFd ) ) 
+        << "Expect false since there are no players."; 
+
+    /* Set a player's echo.  There are no players so this should return false. */
+    EXPECT_EQ(false, ts.setPlayerEcho(playerFd, true) ) 
+        << "Expect false since there are no players.";              
+    
+    /* Shut the server down. */
+    EXPECT_EQ(true, ts.shutdown() ) 
+        << "Expect true that the server is shutdown.";  
+        
+    serverThread.join();          
+}
+
+/* Testing server setters and getters. */
+TEST(ServerTest, ServerSettersandGetters) {
+    legacymud::telnet::Server ts;
+    legacymud::engine::GameLogic gl;
+    int serverPort;
+    int playerCap=1;
+    int timeOut=1;           // timeout in seconds.
+    
+    srand(time(NULL));     // seed the random number generator
+
+    /* All parameters are valid */ 
+    serverPort = rand() % (65535-1000) + 1000;  // range 1000 to 65535
+    
+    ASSERT_EQ(ts.initServer(serverPort, playerCap, timeOut, &gl), true ) 
+            << "Expect true for initializing server.  Could return false if port is already taken.";
+    
+    /* The following only runs if the previous passes. */
+    
+    /* Send listening off to it's own thread. */
+    std::thread serverThread(&legacymud::telnet::Server::startListening, &ts);   
+    
+    /* Set and get server pause. */
+    ts.pause(true);
+    EXPECT_EQ(true, ts.getServerPause() ) 
+        << "Expect pause to be true.";
+    ts.pause(false);
+    EXPECT_EQ(false, ts.getServerPause() ) 
+        << "Expect pause to be true.";        
+
+    /* Set and get maxPlayers. */
+    EXPECT_EQ(false, ts.setMaxPlayers(0) ) 
+        << "Expect false since 0 is invalid.";          // invalid amount
+    EXPECT_EQ(true, ts.setMaxPlayers(10) ) 
+        << "Expect true since 10 is a valid amount.";   // valid amount        
+    EXPECT_EQ(10, ts.getMaxPlayers() ) 
+        << "Expect true the amount was set to 10.";       
+
+    /* Set and get TimeOut. */
+    EXPECT_EQ(false, ts.setTimeOut(0) ) 
+        << "Expect false since 0 is invalid.";          // invalid amount
+    EXPECT_EQ(true, ts.setTimeOut(900) ) 
+        << "Expect true since 10 is a valid amount.";   // valid amount        
+    EXPECT_EQ(900, ts.getTimeOut() ) 
+        << "Expect true the amount was set to 900.";         
+    
+    /* Get the player count. */
+    EXPECT_EQ(0, ts.getPlayerCount() ) 
+        << "Expect 0 since there are no players on the server.";  
+
+    /* Get the server port. */
+    EXPECT_EQ(serverPort, ts.getServerPort() ) 
+        << "Expect true.";   
+
+    /* Get the the game logic pointer. */
+    EXPECT_EQ(&gl, ts.getGameLogicPt() ) 
+        << "Expect true.";         
+    
+    /* Shut the server down. */
+    EXPECT_EQ(true, ts.shutdown() ) 
+        << "Expect true that the server is shutdown.";  
+        
+    serverThread.join();          
+}
 
 }
