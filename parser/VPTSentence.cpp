0d088f0615494f096957a8a447d409ae5fbc1c50
David Rigert <rigertd@onid.orst.edu>
2017-02-17 22:41:20 -0800

    Updated modified date.

--- parser/VPTSentence.cpp
+++ parser/VPTSentence.cpp
@@ -2,7 +2,7 @@
   \file     VPTSentence.cpp
   \author   David Rigert
   \created  02/12/2017
-  \modified 02/16/2017
+  \modified 02/17/2017
   \course   CS467, Winter 2017
  
   \details  This file contains the implementation of the VPTSentence class.
