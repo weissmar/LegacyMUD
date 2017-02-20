d4e7c96c023e104e3efa0dc09b4f1d5a2dcf4de5
David Rigert <rigertd@onid.orst.edu>
2017-02-19 20:17:21 -0800

    Updated warning text.

--- parser/VTPTSentence.hpp
+++ parser/VTPTSentence.hpp
@@ -2,7 +2,7 @@
   \file     VTPTSentence.hpp
   \author   David Rigert
   \created  02/12/2017
-  \modified 02/16/2017
+  \modified 02/19/2017
   \course   CS467, Winter 2017
  
   \details  This file contains the declarations for the VTPTSentence class.
@@ -26,10 +26,9 @@ namespace legacymud { namespace parser {
   \brief Represents a Sentence with a verb, a text-only direct object, a preposition, and a text-only indirect object.
 
   \warning Be careful when using this sentence type.
-           The parser starts from the second-to-last word and moves forward until
-           it finds a matching preposition. If it does not find one, it tries
-           two words at a time, then three, and so on. The first one or more words
-           that match a registered preposition are considered to be the preposition.
+           The parser tries to find the longest matching preposition first,
+           starting with all tokens. If it does not find one, it drops one token
+           each iteration and searches from back to front.
            This may result in the direct and indirect object text being parsed
            incorrectly if that text also includes the preposition text.
 */
